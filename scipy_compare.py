cd6f684fd5c756dbc571a28235058367f0f75e26
Nafeeur <rnafeeur@gmail.com>
2024-11-27 15:42:23 -0500

    Update scipy_compare.py

--- scipy_compare.py
+++ scipy_compare.py
@@ -2,10 +2,11 @@ import numpy as np
 from scipy.signal import find_peaks
 
 # Define the signal
-signal = [0, 2, 1, 3, 0, 1, 2, 1, 5, 7, 8, 1, 3, 0]
+signal = [0, 2, 1, 3, 0, 1, 2, 1, 0]
 
 # Define the parameters
 height = (1.5, np.inf)
+threshold = (0.5, np.inf)
 distance = 1
 prominence = (0.5, np.inf)
 width = (0.0, np.inf)
@@ -15,6 +16,7 @@ rel_height = 0.5
 peaks, properties = find_peaks(
     signal,
     height=height,
+    threshold=threshold,
     distance=distance,
     prominence=prominence,
     width=width,
@@ -25,6 +27,6 @@ peaks, properties = find_peaks(
 print("Peaks at indices:", peaks)
 for i, peak in enumerate(peaks):
     print(f"Peak at index {peak} has height {signal[peak]}, "
+          f"thresholds ({properties['left_thresholds'][i]}, {properties['right_thresholds'][i]}), "
           f"prominence {properties['prominences'][i]}, "
           f"width {properties['widths'][i]}")
-
